85fcb092123783e151a844f308b63e94e549c38d
Victor <victor.as.nascimento@gmail.com>
2021-01-28 10:22:20 -0300

    adding clamping and player movemnet

--- pong_with_rust/src/main.rs
+++ pong_with_rust/src/main.rs
@@ -3,16 +3,29 @@ use ggez::{Context, GameResult};
 use ggez::graphics;
 use ggez::nalgebra as na;
 use ggez::event;
+use ggez::input::keyboard::{self, KeyCode};
 
 const RACKET_HEIGHT: f32 = 100.0;
 const RACKET_WIDTH: f32 = 20.0;
 const RACKET_HEIGHT_HALF: f32 = RACKET_HEIGHT * 0.5;
 const RACKET_WIDTH_HALF: f32 = RACKET_WIDTH * 0.5;
+const BALL_SIZE: f32 = 30.0;
+const BALL_SIZE_HALF: f32 = BALL_SIZE * 0.5;
+const PLAYER_SPEED: f32 = 600.0;
+
+fn clamp(value: &mut f32, low: f32, high: f32) {
+    if *value < low {
+        *value = low;
+    } else if *value > high {
+        *value = high;
+    }
+}
 
 
 struct MainState {
     player_one_pos: na::Point2<f32>,
     player_two_pos: na::Point2<f32>,
+    ball_pos: na::Point2<f32>,
 }
 
 impl MainState {
@@ -22,12 +35,30 @@ impl MainState {
         MainState {
             player_one_pos : na::Point2::new(RACKET_WIDTH_HALF, screen_height_half),
             player_two_pos : na::Point2::new(screen_width - RACKET_HEIGHT_HALF, screen_height_half),
+            ball_pos : na::Point2::new(screen_width_half, screen_height_half),
         }
     }
 }
 
 impl event::EventHandler for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
+        let dt = ggez::timer::delta(ctx).as_secs_f32();
+        let screen_height = graphics::drawable_size(ctx).1;
+
+        if keyboard::is_key_pressed(ctx, KeyCode::W) {
+            self.player_one_pos.y -= PLAYER_SPEED * dt;
+        }
+        if keyboard::is_key_pressed(ctx, KeyCode::S) {
+            self.player_one_pos.y += PLAYER_SPEED * dt;
+        }
+        if keyboard::is_key_pressed(ctx, KeyCode::Up) {
+            self.player_two_pos.y -= PLAYER_SPEED * dt;
+        }
+        if keyboard::is_key_pressed(ctx, KeyCode::Down) {
+            self.player_two_pos.y += PLAYER_SPEED * dt;
+        }
+        clamp(&mut self.player_one_pos.y, RACKET_HEIGHT_HALF, screen_height-RACKET_HEIGHT_HALF);
+        clamp(&mut self.player_two_pos.y, RACKET_HEIGHT_HALF, screen_height-RACKET_HEIGHT_HALF);
         Ok(())
     }
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
@@ -35,6 +66,9 @@ impl event::EventHandler for MainState {
 
         let racket_rect = graphics::Rect::new(-RACKET_WIDTH_HALF, -RACKET_HEIGHT_HALF, RACKET_WIDTH, RACKET_HEIGHT);
         let racket_rect_mesh = graphics::Mesh::new_rectangle(ctx, graphics::DrawMode::stroke(4.0), racket_rect, graphics::WHITE)?;
+
+        let ball_rect = graphics::Rect::new(-BALL_SIZE_HALF, -BALL_SIZE_HALF, BALL_SIZE, BALL_SIZE);
+        let ball_rect_mesh = graphics::Mesh::new_rectangle(ctx, graphics::DrawMode::stroke(4.0), ball_rect, graphics::WHITE)?;
 
         let mut draw_param = graphics::DrawParam::default();
         
@@ -44,6 +78,9 @@ impl event::EventHandler for MainState {
 
         draw_param.dest = self.player_two_pos.into();
         graphics::draw(ctx, &racket_rect_mesh, draw_param);
+
+        draw_param.dest = self.ball_pos.into();
+        graphics::draw(ctx, &ball_rect_mesh, draw_param);
 
         graphics::present(ctx)?;
         Ok(())
